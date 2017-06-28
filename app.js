4eef2988ff9e3bed8dd42c395444d5350d715d9a
salockhart <salexlockhart@gmail.com>
2017-06-28 20:36:33 -0300

    slack functionality

--- app.js
+++ app.js
@@ -62,53 +62,46 @@ app.get('/brain', (req, res) => {
     });
 });
 
-// app.post('/slack', (req, res) => {
-//   if (req.body.token !== process.env.SLACK_VERIFY_TOKEN) {
-//     return res.sendStatus(400);
-//   }
+app.post('/slack', (req, res) => {
+  if (req.body.token !== process.env.SLACK_VERIFY_TOKEN) {
+    return res.sendStatus(400);
+  }
 
-//   let phrase = req.body.text;
-//   let emoji;
+  if (!req.body.text || req.body.text === 'help') {
+    return res.send({
+      response_type: 'ephemeral',
+      text: 'How to use /brain',
+      attachments: [{
+        text: 'To ascend to a higher plane, use `/brain "first brain" "second brain" "third brain" "fourth brain"`',
+      }],
+    });
+  }
 
-//   if (!phrase || phrase === 'help') {
-//     return res.send({
-//       response_type: 'ephemeral',
-//       text: 'How to use /clap',
-//       attachments: [{
-//         text: "To add some claps to your life, use `/clap phrase`\nIf you want to clap without a clap, change it up with `/clap phrase emoji`\nWhichever emoji you put at the end, that's what you'll clap with!",
-//       }],
-//     });
-//   }
+  const [first, second, third, fourth] = req.body.text.split('"').filter(entry => !!entry.trim());
 
-//   const regex = /^(.*?)(:\S*?:)$/g;
-//   const match = regex.exec(phrase);
+  return res.send({
+    response_type: 'in_channel',
+    attachments: [{
+      image_url: `https://expanding-brain-as-a-service.herokuapp.com/brain?first=${first || ''}&second=${second || ''}&third=${third || ''}&fourth=${fourth || ''}`,
+    }],
+  });
+});
 
-//   if (match) {
-//     phrase = match[1];
-//     emoji = match[2];
-//   }
+app.get('/slack/redirect', (req, res) => {
+  const options = {
+    uri: `https://slack.com/api/oauth.access?code=${req.query.code}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
+    method: 'GET',
+  };
 
-//   return res.send({
-//     response_type: 'in_channel',
-//     text: clapPhrase(phrase, emoji),
-//   });
-// });
-
-// app.get('/slack/redirect', (req, res) => {
-//   const options = {
-//     uri: `https://slack.com/api/oauth.access?code=${req.query.code}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
-//     method: 'GET',
-//   };
-
-//   request(options, (error, response, body) => {
-//     const JSONresponse = JSON.parse(body);
-//     if (!JSONresponse.ok) {
-//       res.redirect('http://alexlockhart.ca/expanding-brain-as-a-service/slack/install/?error');
-//     } else {
-//       res.redirect('http://alexlockhart.ca/expanding-brain-as-a-service/slack/install/?success');
-//     }
-//   });
-// });
+  request(options, (error, response, body) => {
+    const JSONresponse = JSON.parse(body);
+    if (!JSONresponse.ok) {
+      res.redirect('http://alexlockhart.ca/expanding-brain-as-a-service/slack/install/?error');
+    } else {
+      res.redirect('http://alexlockhart.ca/expanding-brain-as-a-service/slack/install/?success');
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
